b5c6ee3bc6010c2d860f4bea65d01c52b2b0f1b0
David Kim <kdaeyoub@gmail.com>
2025-06-29 00:52:12 +0900

    Add Windows 98 cursor

--- dist/layout/AppLayout.js
+++ dist/layout/AppLayout.js
@@ -3,5 +3,6 @@ import Wallpaper from '../components/Wallpaper';
 import Desktop from '../components/Desktop/Desktop';
 import WindowArea from '../components/WindowArea/WindowArea';
 import Taskbar from '../components/Taskbar/Taskbar';
-const AppLayout = () => (_jsxs("div", { style: { position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden' }, children: [_jsx(Wallpaper, {}), _jsx(Desktop, {}), _jsx(WindowArea, {}), _jsx(Taskbar, {})] }));
+import PointerTheme from '../components/PointerTheme/PointerTheme';
+const AppLayout = () => (_jsxs("div", { style: { position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden' }, children: [_jsx(Wallpaper, {}), _jsx(Desktop, {}), _jsx(WindowArea, {}), _jsx(Taskbar, {}), _jsx(PointerTheme, {})] }));
 export default AppLayout;
---
+++ src/components/PointerTheme/PointerTheme.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+
+const cursorData =
+  'data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPSczMicgaGVpZ2h0PSczMic+PHBvbHlnb24gZmlsbD0nd2hpdGUnIHN0cm9rZT0nYmxhY2snIHBvaW50cz0nMCwwIDAsMjQgNiwxOCAxMiwzMiAxNiwzMCAxMCwxOCAxOCwxOCcvPjwvc3ZnPg==';
+
+const PointerTheme: React.FC = () => {
+  useEffect(() => {
+    const prev = document.body.style.cursor;
+    document.body.style.cursor = `url(${cursorData}) 0 0, auto`;
+    return () => {
+      document.body.style.cursor = prev;
+    };
+  }, []);
+
+  return null;
+};
+
+export default PointerTheme;
