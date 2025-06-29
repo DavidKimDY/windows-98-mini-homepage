ad44400db794a8539d93a8fb18440b498aa861b6
David Kim <kdaeyoub@gmail.com>
2025-06-30 00:25:37 +0900

    Add start menu toggle and start button icon

--- src/components/Taskbar/StartButton.tsx
+++ src/components/Taskbar/StartButton.tsx
@@ -5,7 +5,10 @@ interface Props {
 }
 
 const StartButton: React.FC<Props> = ({ onClick }) => (
-  <button type="button" className="start-button" onClick={onClick}>시작</button>
+  <button type="button" className="start-button" onClick={onClick}>
+    <img className="start-icon" src="./images/windows-logo.png" alt="" />
+    시작
+  </button>
 );
 
 export default StartButton;
