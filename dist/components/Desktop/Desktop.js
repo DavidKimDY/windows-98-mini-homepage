efdd2f1ae278234877b4073f207e605bc2ce1fd8
David Kim <kdaeyoub@gmail.com>
2025-06-29 23:55:46 +0900

    Use PNG icons

--- dist/components/Desktop/Desktop.js
+++ dist/components/Desktop/Desktop.js
@@ -4,10 +4,10 @@ const Desktop = () => (
   _jsxs("div", {
     className: "desktop",
     children: [
-      _jsx(DesktopIcon, { label: "\uB0B4 \uCEF4\uD4E8\uD130", icon: "./images/my-computer.svg" }),
-      _jsx(DesktopIcon, { label: "\uB0B4 \uBB38\uC11C", icon: "./images/my-docs.svg" }),
-      _jsx(DesktopIcon, { label: "\uC778\uD130\uB137", icon: "./images/internet.svg" }),
-      _jsx(DesktopIcon, { label: "\uC774\uBA54\uC77C", icon: "./images/my-email.svg", href: "mailto:kdaeyoub@gmail.com" })
+      _jsx(DesktopIcon, { label: "\uB0B4 \uCEF4\uD4E8\uD130", icon: "./images/my-computer.png" }),
+      _jsx(DesktopIcon, { label: "\uB0B4 \uBB38\uC11C", icon: "./images/my-document.png" }),
+      _jsx(DesktopIcon, { label: "\uC778\uD130\uB137", icon: "./images/internet.png" }),
+      _jsx(DesktopIcon, { label: "\uC774\uBA54\uC77C", icon: "./images/my-email.png", href: "mailto:kdaeyoub@gmail.com" })
     ]
   })
 );
--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -3,10 +3,10 @@ import DesktopIcon from '../DesktopIcon/DesktopIcon';
 
 const Desktop: React.FC = () => (
   <div className="desktop">
-    <DesktopIcon label="내 컴퓨터" icon="./images/my-computer.svg" />
-    <DesktopIcon label="내 문서" icon="./images/my-docs.svg" />
-    <DesktopIcon label="인터넷" icon="./images/internet.svg" />
-    <DesktopIcon label="이메일" icon="./images/my-email.svg" href="mailto:kdaeyoub@gmail.com" />
+    <DesktopIcon label="내 컴퓨터" icon="./images/my-computer.png" />
+    <DesktopIcon label="내 문서" icon="./images/my-document.png" />
+    <DesktopIcon label="인터넷" icon="./images/internet.png" />
+    <DesktopIcon label="이메일" icon="./images/my-email.png" href="mailto:kdaeyoub@gmail.com" />
   </div>
 );
 
