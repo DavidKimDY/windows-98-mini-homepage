aa6d695ec3f0e7671f4de081c885ecbb5a70bd2b
David Kim <kdaeyoub@gmail.com>
2025-06-29 23:05:08 +0900

    Fix JS module paths

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import ReactDOM from 'react-dom/client';
-import AppLayout from './layout/AppLayout';
+import AppLayout from './layout/AppLayout.js';
 const rootEl = document.getElementById('root');
 if (rootEl) {
     const root = ReactDOM.createRoot(rootEl);
