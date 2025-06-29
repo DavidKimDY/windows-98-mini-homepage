62b583637bea93b9ea576f760fe48bfd3c6b6962
David Kim <kdaeyoub@gmail.com>
2025-06-30 00:23:04 +0900

    Update Desktop.js

--- dist/components/Desktop/Desktop.js
+++ dist/components/Desktop/Desktop.js
@@ -7,7 +7,7 @@ const Desktop = () => (
       _jsx(DesktopIcon, { label: "\uB0B4 \uCEF4\uD4E8\uD130", icon: "./images/my-computer.png" }),
       _jsx(DesktopIcon, { label: "\uB0B4 \uBB38\uC11C", icon: "./images/my-document.png" }),
       _jsx(DesktopIcon, { label: "\uC778\uD130\uB137", icon: "./images/internet.png" }),
-      _jsx(DesktopIcon, { label: "\uC774\uBA54\uC77C", icon: "./images/my-email.png", href: "mailto:kdaeyoub@gmail.com" })
+      _jsx(DesktopIcon, { label: "\uC774\uBA54\uC77C", icon: "./images/email.png", href: "mailto:kdaeyoub@gmail.com" })
     ]
   })
 );
