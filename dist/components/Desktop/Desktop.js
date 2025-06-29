726f19d4fe4b12fece264b9e59bda9ff1e67f58f
David Kim <kdaeyoub@gmail.com>
2025-06-29 23:05:24 +0900

    Fix JS module paths

--- dist/components/Desktop/Desktop.js
+++ dist/components/Desktop/Desktop.js
@@ -1,4 +1,4 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import DesktopIcon from '../DesktopIcon/DesktopIcon';
+import DesktopIcon from '../DesktopIcon/DesktopIcon.js';
 const Desktop = () => (_jsxs("div", { className: "desktop", children: [_jsx(DesktopIcon, { label: "\uB0B4 \uCEF4\uD4E8\uD130" }), _jsx(DesktopIcon, { label: "\uB0B4 \uBB38\uC11C" }), _jsx(DesktopIcon, { label: "\uC778\uD130\uB137" }), _jsx(DesktopIcon, { label: "Notepad" })] }));
 export default Desktop;
